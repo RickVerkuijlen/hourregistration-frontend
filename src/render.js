05d50d4955b4aafcfa818cea56c951f7ebad9918
RickVerkuijlen <i410126@fhict.nl>
2020-06-30 16:58:24 +0200

    Tried some styling

--- src/render.js
+++ src/render.js
@@ -1,35 +1,21 @@
 var fs = require('fs');
-const { remote } = require('electron');
-const { Menu } = remote;
-
-// const { getAllProjects }  = require('./services/api.service.js');
 
 const testFolder = "V:\\Tekeningen";
-const nogTest = "C:/Users/rickv/Desktop";
 var directories = [];
-
-document.getElementById('showDirs').addEventListener("click", function() {
-    fs.mkdir(nogTest + "/test", { recursive: true}, (err) => {
-        err ? console.log(err) : null
-    })
-});
+var projects = [];
 
 
+directories = fs.readdirSync(testFolder, { withFileTypes: true})
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name);
 
-fs.readdirSync(testFolder).forEach(file => {
-    console.log(file);
-    directories.push(file);
+    var projectDiv = document.getElementById('project');
+
+getAllProjects().then(res => {
+   res.forEach(project => {
+        var title = document.createElement('H1');
+        var text = document.createTextNode(project.code);
+        title.innerHTML = project.code;
+        projectDiv.appendChild(title);
+   })
 })
-
-
-const directoriesMenu = Menu.buildFromTemplate(
-    directories.map(file => {
-        console.log(file);
-        return {
-            label: file,
-            click: () => selectSource()
-        }
-    })
-)
-
-console.log(getAllProjects());
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -4,8 +4,6 @@ const axios = require('axios');
 var _baseUrl = process.env.API_URL;
 
 async function getAllProjects() {
-
-    console.log(this._baseUrl);
     return await axios.get(this._baseUrl + "projects")
     .then(response => {
         return response.data;
